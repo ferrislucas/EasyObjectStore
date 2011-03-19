f7057038282b8038f47a806988d58bbdce559ce9
Ferris Lucas <lucasf@digitalev.com>
2011-03-18 22:12:59 -0500

    implement EasyObjectStoreFactory

---
+++ src/EasyObjectStore/EasyObjectStoreFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EasyObjectStore.Helpers;
+
+namespace EasyObjectStore
+{
+	public class EasyObjectStoreFactory<T>
+	{
+		public static EasyObjectStore<T> GetInstance()
+		{
+			return new EasyObjectStore<T>(new XmlFileSerializationHelper(), 
+											new GetDataPathForType(new GetPathToDataDirectoryService(new ApplicationSettingsValueGetter())), 
+											new GetValueOfIdPropertyForInstance(new GetNameOfIdPropertyForType()), 
+											new GuidGetter(), 
+											new FileSystem());
+		}
+
+		public static EasyObjectStore<T> GetInstance(string pathToDataDirectory)
+		{
+			return new EasyObjectStore<T>(new XmlFileSerializationHelper(),
+											new GetDataPathForType(new GetHardCodedPathToDataDirectoryService(pathToDataDirectory)),
+											new GetValueOfIdPropertyForInstance(new GetNameOfIdPropertyForType()),
+											new GuidGetter(),
+											new FileSystem());
+		}
+
+		private class GetHardCodedPathToDataDirectoryService : IGetPathToDataDirectoryService
+		{
+			private readonly string pathToDataDirectory;
+
+			public GetHardCodedPathToDataDirectoryService(string pathToDataDirectory)
+			{
+				this.pathToDataDirectory = pathToDataDirectory;
+			}
+
+			public string GetPathToDirectory()
+			{
+				return pathToDataDirectory;
+			}
+		}
+	}
+}
